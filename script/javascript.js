b3fdde3a4a9d9d196de1c51c58871f63749d38df
Plasma-Adry <162125280+Plasma-Adry@users.noreply.github.com>
2024-04-01 14:02:29 +0200

    Update javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -1,1 +1,11 @@
-
+<script>
+ function doLogin() {
+  var Username = document.getElementbyId("Username").value;
+  var Password = document.getElementbyId("Password").value;
+  if(Username == "" || Password == "") {
+    alert("Username e Password sono obbligatori");
+  }
+ }
+</script>
+    
+  
